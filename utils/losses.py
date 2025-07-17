0863c65b08790bb703985ba949a94e3e0489026e
dhhdy <122719285+dhhdy@users.noreply.github.com>
2025-07-17 21:14:58 +0800

    Add files via upload

---
+++ utils/losses.py
@@ -0,0 +1,313 @@
+import torch
+from torch.nn import functional as F
+import numpy as np
+import torch.nn as nn
+
+import torch
+import torch.nn.functional as F
+
+def to_one_hot(tensor, nClasses):
+    """ Input tensor : Nx1xHxW
+    :param tensor:
+    :param nClasses:
+    :return:
+    """
+    assert tensor.max().item() < nClasses, 'one hot tensor.max() = {} < {}'.format(torch.max(tensor), nClasses)
+    assert tensor.min().item() >= 0, 'one hot tensor.min() = {} < {}'.format(tensor.min(), 0)
+
+    size = list(tensor.size())
+    assert size[1] == 1
+    size[1] = nClasses
+    one_hot = torch.zeros(*size)
+    if tensor.is_cuda:
+        one_hot = one_hot.cuda(tensor.device)
+    one_hot = one_hot.scatter_(1, tensor, 1)
+    return one_hot
+
+def get_probability(logits):
+    """ Get probability from logits, if the channel of logits is 1 then use sigmoid else use softmax.
+    :param logits: [N, C, H, W] or [N, C, D, H, W]
+    :return: prediction and class num
+    """
+    size = logits.size()
+    # N x 1 x H x W
+    if size[1] > 1:
+        pred = F.softmax(logits, dim=1)
+        nclass = size[1]
+    else:
+        pred = F.sigmoid(logits)
+        pred = torch.cat([1 - pred, pred], 1)
+        nclass = 2
+    return pred, nclass
+
+class mask_DiceLoss(nn.Module):
+    def __init__(self, nclass, class_weights=None, smooth=1e-5):
+        super(mask_DiceLoss, self).__init__()
+        self.smooth = smooth
+        if class_weights is None:
+            # default weight is all 1
+            self.class_weights = nn.Parameter(torch.ones((1, nclass)).type(torch.float32), requires_grad=False)
+        else:
+            class_weights = np.array(class_weights)
+            assert nclass == class_weights.shape[0]
+            self.class_weights = nn.Parameter(torch.tensor(class_weights, dtype=torch.float32), requires_grad=False)
+
+    def prob_forward(self, pred, target, mask=None):
+        size = pred.size()
+        N, nclass = size[0], size[1]
+        # N x C x H x W
+        pred_one_hot = pred.view(N, nclass, -1)
+        target = target.view(N, 1, -1)
+        target_one_hot = to_one_hot(target.type(torch.long), nclass).type(torch.float32)
+
+        # N x C x H x W
+        inter = pred_one_hot * target_one_hot
+        union = pred_one_hot + target_one_hot
+
+        if mask is not None:
+            mask = mask.view(N, 1, -1)
+            inter = (inter.view(N, nclass, -1) * mask).sum(2)
+            union = (union.view(N, nclass, -1) * mask).sum(2)
+        else:
+            # N x C
+            inter = inter.view(N, nclass, -1).sum(2)
+            union = union.view(N, nclass, -1).sum(2)
+
+        # smooth to prevent overfitting
+        # [https://github.com/pytorch/pytorch/issues/1249]
+        # NxC
+        dice = (2 * inter + self.smooth) / (union + self.smooth)
+        return 1 - dice.mean()
+
+    def forward(self, logits, target, mask=None):
+        size = logits.size()
+        N, nclass = size[0], size[1]
+
+        logits = logits.view(N, nclass, -1)
+        target = target.view(N, 1, -1)
+
+        pred, nclass = get_probability(logits)
+
+        # N x C x H x W
+        pred_one_hot = pred
+        target_one_hot = to_one_hot(target.type(torch.long), nclass).type(torch.float32)
+
+        # N x C x H x W
+        inter = pred_one_hot * target_one_hot
+        union = pred_one_hot + target_one_hot
+
+        if mask is not None:
+            mask = mask.view(N, 1, -1)
+            inter = (inter.view(N, nclass, -1) * mask).sum(2)
+            union = (union.view(N, nclass, -1) * mask).sum(2)
+        else:
+            # N x C
+            inter = inter.view(N, nclass, -1).sum(2)
+            union = union.view(N, nclass, -1).sum(2)
+
+        # smooth to prevent overfitting
+        # [https://github.com/pytorch/pytorch/issues/1249]
+        # NxC
+        dice = (2 * inter + self.smooth) / (union + self.smooth)
+        return 1 - dice.mean()
+
+def weighted_cross_entropy(pred, target, weight_map):
+    """
+    pred: 预测值，形状 [N, C, H, W]
+    target: 真实标签，形状 [N, H, W]，类别的索引
+    weight_map: 权重矩阵，形状 [N, H, W]
+    """
+    # print('WWW:',pred.dtype, target.dtype, weight_map.dtype)  # 可能输出 torch.float16, torch.long, torch.float32
+    N = pred.shape[1]
+    if N > 1:
+        log_softmax = F.log_softmax(pred, dim=1)
+        # 获取对应类别的log概率
+        log_softmax = log_softmax.gather(1, target.unsqueeze(1)).squeeze(1)  # [N, H, W]
+        loss = -log_softmax * weight_map
+    else:
+        pred_sigmoid = torch.sigmoid(pred.squeeze(1))  # [N, H, W]
+        loss = - (target * torch.log(pred_sigmoid + 1e-8) +
+                  (1 - target) * torch.log(1 - pred_sigmoid + 1e-8))
+        loss = loss * weight_map
+    return loss.mean()  # 平均损失
+
+def weighted_dice_loss(pred, target, weight_map, smooth=1e-8):
+    """
+    pred: logit, 形状 [N, C, H, W, D]
+    target: 形状 [N, H, W, D]
+    weight_map: 权重矩阵，形状 [N, H, W, D]
+    """
+    # print('WWD:',pred.dtype, target.dtype, weight_map.dtype)  # 可能输出 torch.float16, torch.long, torch.float32
+    N = pred.shape[1]
+    if N > 1:
+        pred = F.softmax(pred, dim=1)
+    else:
+        pred = F.sigmoid(pred)
+        pred = torch.cat((1-pred, pred), dim=1)
+    target = F.one_hot(target, num_classes=2).permute(0, 4, 1, 2, 3)  # 【N，C，H，W, D]
+    weight_map = weight_map.unsqueeze(1)  # [N, 1, H, W，D]
+    # 计算加权交集和并集
+    intersection = (pred * target * weight_map).sum(dim=(2, 3, 4))  # [N, C]
+    union = (pred * weight_map).sum(dim=(2, 3, 4)) + (target * weight_map).sum(dim=(2, 3, 4))
+    dice = (2. * intersection + smooth) / (union + smooth)
+    return 1 - dice.mean()  # 平均类别和样本
+
+# 示例用法：
+# 生成one-hot标签和权重
+# target_one_hot = F.one_hot(target, num_classes=C).permute(0, 3, 1, 2)
+# weight_map = generate_weight_map(target)  # 自定义权重生成逻辑
+# loss = weighted_dice_loss(pred_softmax, target_one_hot, weight_map)
+
+def dice_loss(score, target):
+    target = target.float()
+    smooth = 1e-5
+    intersect = torch.sum(score * target)
+    y_sum = torch.sum(target * target)
+    z_sum = torch.sum(score * score)
+    loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+    loss = 1 - loss
+    return loss
+
+def dice_loss1(score, target):
+    target = target.float()
+    smooth = 1e-5
+    intersect = torch.sum(score * target)
+    y_sum = torch.sum(target)
+    z_sum = torch.sum(score)
+    loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+    loss = 1 - loss
+    return loss
+
+# def entropy_loss(p,C=2):
+#     ## p N*C*W*H*D
+#     y1 = -1*torch.sum(p*torch.log(p+1e-6), dim=1)/torch.tensor(np.log(C)).cuda()
+#     ent = torch.mean(y1)
+#
+#     return ent
+
+def unlabel_mask_entropy(p, mask):
+    p = F.sigmoid(p)
+    ent = -torch.sum(p * torch.log(p + 1e-8) * mask, dim=1)
+    ent = torch.mean(ent)
+
+    return ent
+
+def softmax_dice_loss(input_logits, target_logits):
+    """Takes softmax on both sides and returns MSE loss
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert input_logits.size() == target_logits.size()
+    input_softmax = F.softmax(input_logits, dim=1)
+    target_softmax = F.softmax(target_logits, dim=1)
+    n = input_logits.shape[1]
+    dice = 0
+    for i in range(0, n):
+        dice += dice_loss1(input_softmax[:, i], target_softmax[:, i])
+    mean_dice = dice / n
+
+    return mean_dice
+
+
+def entropy_loss_map(p, C=2):
+    ent = -1*torch.sum(p * torch.log(p + 1e-6), dim=1, keepdim=True)/torch.tensor(np.log(C)).cuda()
+    return ent
+
+def softmax_mse_loss(input_logits, target_logits):
+    """Takes softmax on both sides and returns MSE loss
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert input_logits.size() == target_logits.size()
+    input_softmax = F.softmax(input_logits, dim=1)
+    target_softmax = F.softmax(target_logits, dim=1)
+
+    mse_loss = (input_softmax-target_softmax)**2
+    return mse_loss
+
+def sigmoid_mse_loss(input_logits, target_logits):
+    """Takes softmax on both sides and returns MSE loss
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert input_logits.size() == target_logits.size()
+    input_softmax = F.sigmoid(input_logits)
+    target_softmax = F.sigmoid(target_logits)
+
+    mse_loss = (input_softmax-target_softmax)**2
+    return mse_loss
+
+def softmax_kl_loss(input_logits, target_logits):
+    """Takes softmax on both sides and returns KL divergence
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to inputs but not the targets.
+    """
+    assert input_logits.size() == target_logits.size()
+    input_log_softmax = F.log_softmax(input_logits, dim=1)
+    target_softmax = F.softmax(target_logits, dim=1)
+
+    # return F.kl_div(input_log_softmax, target_softmax)
+    kl_div = F.kl_div(input_log_softmax, target_softmax, reduction='none')
+    # mean_kl_div = torch.mean(0.2*kl_div[:,0,...]+0.8*kl_div[:,1,...])
+    return kl_div
+
+def symmetric_mse_loss(input1, input2):
+    """Like F.mse_loss but sends gradients to both directions
+
+    Note:
+    - Returns the sum over all examples. Divide by the batch size afterwards
+      if you want the mean.
+    - Sends gradients to both input1 and input2.
+    """
+    assert input1.size() == input2.size()
+    return torch.mean((input1 - input2)**2)
+
+class DiceLoss(nn.Module):
+    def __init__(self, n_classes):
+        super(DiceLoss, self).__init__()
+        self.n_classes = n_classes
+
+    def _one_hot_encoder(self, input_tensor):
+        tensor_list = []
+        for i in range(self.n_classes):
+            temp_prob = input_tensor == i * torch.ones_like(input_tensor)
+            tensor_list.append(temp_prob)
+        output_tensor = torch.cat(tensor_list, dim=1)
+        return output_tensor.float()
+
+    def _dice_loss(self, score, target):
+        target = target.float()
+        smooth = 1e-5
+        intersect = torch.sum(score * target)
+        y_sum = torch.sum(target * target)
+        z_sum = torch.sum(score * score)
+        loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+        loss = 1 - loss
+        return loss
+
+    def forward(self, inputs, target, weight=None, softmax=False):
+        if softmax:
+            inputs = torch.softmax(inputs, dim=1)
+        target = self._one_hot_encoder(target)
+        if weight is None:
+            weight = [1] * self.n_classes
+        assert inputs.size() == target.size(), 'predict & target shape do not match'
+        class_wise_dice = []
+        loss = 0.0
+        for i in range(0, self.n_classes):
+            dice = self._dice_loss(inputs[:, i], target[:, i])
+            class_wise_dice.append(1.0 - dice.item())
+            loss += dice * weight[i]
+        return loss / self.n_classes
